17ea7b61ef956191b124d378358a12421276fda8
DESKTOP-EUH5AVR\DELL <narendarshekhawat4@gmail.com>
2022-01-23 15:10:30 +0530

    jhj

--- Springboot-Backend/src/main/java/com/discussionforum/auth/security/jwt/JwtAuthEntryPoint.java
+++ Springboot-Backend/src/main/java/com/discussionforum/auth/security/jwt/JwtAuthEntryPoint.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
 
-    private static final Logger logger = LoggerFactory.getLogger(JwtAuthEntryPoint.class);
+	private static final Logger logger = LoggerFactory.getLogger(JwtAuthEntryPoint.class);
     
     @Override
     public void commence(HttpServletRequest request,
